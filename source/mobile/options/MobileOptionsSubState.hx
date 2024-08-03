2a0d422ca4a1837b832c4327b672f58924f32d81
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-08-03 22:20:12 +0800

    Boo
    This is not needed

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -36,9 +36,6 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		var option:Option = new Option('Hide Hitbox Hints', 'If checked, makes the hitbox invisible.', 'hideHitboxHints', 'bool');
 		addOption(option);
 
-		var option:Option = new Option('Hitbox Position', 'If checked, the hitbox will be put at the bottom of the screen, otherwise will stay at the top.', 'hitbox2', 'bool');
-		addOption(option);
-
 		var option:Option = new Option('Dynamic Controls Color', 'If checked, the mobile controls color will be set to the notes color in your settings.\n(have effect during gameplay only)', 'dynamicColors', 'bool');
 		addOption(option);
 
