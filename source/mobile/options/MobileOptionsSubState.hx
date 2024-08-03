a518c45b107c6aedcb678f532283c45607e28e6f
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-08-03 22:02:42 +0800

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -5,10 +5,15 @@ import options.Option;
 
 class MobileOptionsSubState extends BaseOptionsMenu
 {
-	var exControlTypes:Array<String> = ["NONE", "SINGLE", "DOUBLE"];
+	#if android
+	var storageTypes:Array<String> = [/*"CUSTOM", */ "EXTERNAL_DATA", "EXTERNAL_OBB", "EXTERNAL_MEDIA", "EXTERNAL"];
+	var externalPaths:Array<String> = SUtil.checkExternalPaths(true);
+	final lastStorageType:String = ClientPrefs.data.storageType;
+	#end
 
 	public function new()
 	{
+		#if android if (!externalPaths.contains('\n')) storageTypes = storageTypes.concat(externalPaths); #end
 		title = 'Mobile Options';
 		rpcTitle = 'Mobile Options Menu'; // for Discord Rich Presence, fuck it
 
@@ -18,6 +23,12 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		{
 			lime.system.System.allowScreenTimeout = curOption.getValue();
 		};
+		addOption(option);
+
+		var option:Option = new Option('Wide Screen Mode',
+			'If checked, The game will stetch to fill your whole screen. (WARNING: Can result in bad visuals & break some mods that resizes the game/cameras)',
+			'wideScreen', 'bool');
+		option.onChange = () -> FlxG.scaleMode = new MobileScaleMode();
 		addOption(option);
 		#end
 
@@ -30,6 +41,39 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		var option:Option = new Option('Dynamic Controls Color', 'If checked, the mobile controls color will be set to the notes color in your settings.\n(have effect during gameplay only)', 'dynamicColors', 'bool');
 		addOption(option);
 
+		#if android
+		 var option:Option = new Option('Storage Type', 'Which folder Psych Engine should use?\n(CHANGING THIS MAKES DELETE YOUR OLD FOLDER!!)', 'storageType', STRING,
+			storageTypes);
+		addOption(option);
+		#end
+
 		super();
+	}
+	#if android
+	function onStorageChange():Void
+	{
+		File.saveContent(lime.system.System.applicationStorageDirectory + 'storagetype.txt', ClientPrefs.data.storageType);
+
+		var lastStoragePath:String = StorageType.fromStrForce(lastStorageType) + '/';
+
+		try
+		{
+			Sys.command('rm', ['-rf', lastStoragePath]);
+		}
+		catch (e:haxe.Exception)
+			trace('Failed to remove last directory. (${e.message})');
+	}
+	#end
+	override public function destroy()
+	{
+		super.destroy();
+		#if android
+		if (ClientPrefs.data.storageType != lastStorageType)
+		{
+			onStorageChange();
+			SUtil.showPopUp('Storage Type has been changed and you needed restart the game!!\nPress OK to close the game.', 'Notice!');
+			lime.system.System.exit(0);
+		}
+		#end
 	}
 }
