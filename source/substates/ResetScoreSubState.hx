19ce0df3794946b031ed413e66a5dd1524a5dddc
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-08-06 16:59:11 +0800

    uh
    ok

--- source/substates/ResetScoreSubState.hx
+++ source/substates/ResetScoreSubState.hx
@@ -88,7 +88,7 @@ class ResetScoreSubState extends MusicBeatSubstate
 		}
 		if(week == -1) icon.alpha += elapsed * 2.5;
 
-		if(controls.UI_LEFT_P || controls.UI_RIGHT_P #if mobile _virtualpad.buttonLeft.justPressed || _virtualpad.buttonRight.justPressed #end) {
+		if(controls.UI_LEFT_P || controls.UI_RIGHT_P  #if mobile || _virtualpad.buttonLeft.justPressed || _virtualpad.buttonRight.justPressed #end) {
 			FlxG.sound.play(Paths.sound('scrollMenu'), 1);
 			onYes = !onYes;
 			updateOptions();
