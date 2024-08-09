cbab7630c0f9d6d883795aa199cb603ccecb8d3c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-08-09 11:45:24 +0800

    Update BaseOptionsMenu.hx
    None of the options are saving changes rn for some reason so adding this to all the option stuff should fix it

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -144,7 +144,8 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		}
 
 		if (controls.BACK #if mobile || _virtualpad.buttonB.justPressed #end) {
-			#if mobile
+			ClientPrefs.saveSettings();
+		        #if mobile
 			closeSs();
 			#else
 			close();
