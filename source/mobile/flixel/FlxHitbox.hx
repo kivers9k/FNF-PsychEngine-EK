397aeb5d50c8fa161fbaa341574f55b73c3fdf2b
kivers69 <georgechen1297@gmail.com>
2024-08-11 14:31:32 +0800

    Add files via upload

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -11,20 +11,7 @@ import openfl.display.Shape;
 
 class FlxHitbox extends FlxSpriteGroup {
 	public var hitbox:FlxSpriteGroup;
-
 	public var array:Array<FlxButton> = [];
-
-	var hitboxColor:Map<Int, Array<Int>> = [
-		1 => [0xffFFFFFF],
-		2 => [0xffE390E6, 0xffFF0000],
-		3 => [0xffE390E6, 0xffFFFFFF, 0xffFF0000],
-		4 => [0xffE390E6, 0xff00EDFF, 0xff00FF00, 0xffFF0000],
-		5 => [0xffE390E6, 0xff00EDFF, 0xffFFFFFF, 0xff00FF00, 0xffFF0000],
-		6 => [0xffE390E6, 0xff00FF00, 0xffFF0000, 0xffFFFF00, 0xff00EDFF, 0xff0000FF],
-		7 => [0xffE390E6, 0xff00FF00, 0xffFF0000, 0xffFFFFFF, 0xffFFFF00, 0xff00EDFF, 0xff0000FF],
-		8 => [0xffE390E6, 0xff00EDFF, 0xff00FF00, 0xffFF0000, 0xffFFFF00, 0xffBB00FF, 0xffFF0000, 0xff0000FF],
-		9 => [0xffE390E6, 0xff00EDFF, 0xff00FF00, 0xffFF0000, 0xffFFFFFF, 0xffFFFF00, 0xffBB00FF, 0xffFF0000, 0xff0000FF],
- 	];
 
 	public function new(?type:Int = 3) {
 		super();
@@ -32,8 +19,9 @@ class FlxHitbox extends FlxSpriteGroup {
 		
 		var keyCount:Int = type + 1;
 		var hitboxWidth:Int = Math.floor(FlxG.width / keyCount);
+		var hitboxColor:ExtraKeysHandler = ExtraKeysHandler.instance.data.hitboxColor[type];
 		for (i in 0 ... keyCount) {
-			hitbox.add(add(array[i] = createhitbox(hitboxWidth * i, 0, hitboxWidth, FlxG.height, hitboxColor[keyCount][i])));
+			hitbox.add(add(array[i] = createhitbox(hitboxWidth * i, 0, hitboxWidth, FlxG.height, hitboxColor[i])));
 		}
 	}
 
@@ -94,13 +82,6 @@ class FlxHitbox extends FlxSpriteGroup {
 		return button;
 	}
 
-
-	override public function destroy():Void {
-		super.destroy();
-		for (hbox in array) {
-			hbox = null;
-		}
-	}
 	function createHintGraphic(Width:Int, Height:Int):BitmapData
 	{
 		var guh = ClientPrefs.data.controlsAlpha;
@@ -119,5 +100,13 @@ class FlxHitbox extends FlxSpriteGroup {
 		var bitmap:BitmapData = new BitmapData(Width, Height, true, 0);
 		bitmap.draw(shape);
 		return bitmap;
+	}
+
+	override public function destroy():Void {
+		super.destroy();
+		hitbox = FlxDestroyUtil.destroy(hitbox);
+		for (hbox in array) {
+			hbox = null;
+		}
 	}
 }
