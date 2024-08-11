61d2526e4e1df8e1a73c3d12ab07a8e7c63df0a3
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-08-11 13:03:37 +0800

    I forgor to add exit 💀

--- source/states/editors/NoteSplashDebugState.hx
+++ source/states/editors/NoteSplashDebugState.hx
@@ -193,7 +193,7 @@ class NoteSplashDebugState extends MusicBeatState
 		cast(stepperMinFps.text_field, FlxInputText).hasFocus = cast(stepperMaxFps.text_field, FlxInputText).hasFocus = false;
 
 		var notTyping:Bool = !nameInputText.hasFocus && !imageInputText.hasFocus;
-		if(controls.BACK && notTyping)
+		if(controls.BACK #if mobile || _virtualpad.buttonB.justPressed || FlxG.android.justReleased.BACK #end && notTyping)
 		{
 			MusicBeatState.switchState(new MasterEditorMenu());
 			FlxG.sound.playMusic(Paths.music('freakyMenu'));
