8af3a49d311774b6d3ad92849ffac021e70ec37d
kivers69 <georgechen1297@gmail.com>
2024-08-13 18:42:04 +0800

    bej3jej4vejiwhdksjskshwohe

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -11,6 +11,8 @@ import openfl.display.BitmapData;
 import openfl.display.Shape;
 import backend.ExtraKeysHandler;
 
+using StringTools;
+
 class FlxHitbox extends FlxSpriteGroup {
 	public var hitbox:FlxSpriteGroup;
 	public var array:Array<FlxButton> = [];
@@ -27,59 +29,16 @@ class FlxHitbox extends FlxSpriteGroup {
 	}
 
 	public function createhitbox(x:Float = 0, y:Float = 0, width:Int, height:Int, color:String) {
-
-		var hintTween:FlxTween = null;
 		var button:FlxButton = new FlxButton(x, y);
 		button.loadGraphic(createHintGraphic(width, height));
-		button.color = FlxColor.fromString(color);
 		button.updateHitbox();
-		button.alpha = 0.00001;
+		button.alpha = 0;
+		if (!color.startWiths('0x')) button.color = FlxColor.fromString('0x' + color);
 
-		if (!ClientPrefs.data.hideHitboxHints)
-		{
-			button.onDown.callback = function()
-			{
-				if (hintTween != null)
-					hintTween.cancel();
+		button.onDown.callback = function() button.alpha = ClientPrefs.data.controlsAlpha;
+		button.onUp.callback = function() button.alpha = 0;
+		button.onOut.callback = function() button.alpha = 0;
 
-				hintTween = FlxTween.tween(button, {alpha: ClientPrefs.data.controlsAlpha}, ClientPrefs.data.controlsAlpha / 100, {
-					ease: FlxEase.circInOut,
-					onComplete: function(twn:FlxTween)
-					{
-						hintTween = null;
-					}
-				});
-			}
-			button.onUp.callback = function()
-			{
-				if (hintTween != null)
-					hintTween.cancel();
-
-				hintTween = FlxTween.tween(button, {alpha: 0.00001}, ClientPrefs.data.controlsAlpha / 10, {
-					ease: FlxEase.circInOut,
-					onComplete: function(twn:FlxTween)
-					{
-						hintTween = null;
-					}
-				});
-			}
-			button.onOut.callback = function()
-			{
-				if (hintTween != null)
-					hintTween.cancel();
-
-				hintTween = FlxTween.tween(button, {alpha: 0.00001}, ClientPrefs.data.controlsAlpha / 10, {
-					ease: FlxEase.circInOut,
-					onComplete: function(twn:FlxTween)
-					{
-						hintTween = null;
-					}
-				});
-			}
-		}
-		#if FLX_DEBUG
-		hint.ignoreDrawDebug = true;
-		#end
 		return button;
 	}
 
@@ -88,18 +47,24 @@ class FlxHitbox extends FlxSpriteGroup {
 		var guh = ClientPrefs.data.controlsAlpha;
 		if (guh >= 0.9)
 			guh = ClientPrefs.data.controlsAlpha - 0.07;
+
 		var shape:Shape = new Shape();
-		shape.graphics.beginFill(0xFFFFFF);
-		shape.graphics.lineStyle(3, 0xFFFFFF, 1);
-		shape.graphics.drawRect(0, 0, Width, Height);
-		shape.graphics.lineStyle(0, 0, 0);
-		shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
-		shape.graphics.endFill();
 		shape.graphics.beginGradientFill(RADIAL, [0xFFFFFF, FlxColor.TRANSPARENT], [guh, 0], [0, 255], null, null, null, 0.5);
 		shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
 		shape.graphics.endFill();
+
+		if (!ClientPrefs.data.hideHitboxHints) {
+			shape.graphics.beginFill(0xFFFFFF);
+			shape.graphics.lineStyle(3, 0xFFFFFF, 1);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.lineStyle(0, 0, 0);
+			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
+			shape.graphics.endFill();
+		}
+
 		var bitmap:BitmapData = new BitmapData(Width, Height, true, 0);
 		bitmap.draw(shape);
+
 		return bitmap;
 	}
 
